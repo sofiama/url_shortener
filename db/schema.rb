de51fb7929c8f9cd4ec91e1c19fb6461aff96d18
sofia ma <sofia.ma@gmail.com>
2015-03-19 00:11:44 -0400

    changed db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150319031110) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: true do |t|
     t.string   "original"
     t.string   "short"
